9dd9829b560983ff35d1bc8c14e0915246c92775
mmraihan <mm_raihan@live.com>
2023-06-07 21:11:30 +0600

    Master Details: Added PurchaseOrderDetail Form in POHedaer Create UI.

--- InventoryManagementSystem/InventoryManagementSystem/Controllers/PurchaseOrderController.cs
+++ InventoryManagementSystem/InventoryManagementSystem/Controllers/PurchaseOrderController.cs
@@ -2,16 +2,21 @@
 using InventoryManagementSystem.Models;
 using InventoryManagementSystem.Tools;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace InventoryManagementSystem.Controllers
 {
     public class PurchaseOrderController : Controller
     {
         private readonly IPurchaseOrder _purchaseOrderRepo;
-        public PurchaseOrderController(IPurchaseOrder purchaseOrderRepo)
+        private readonly IProduct _productRepo;
+        public PurchaseOrderController(IPurchaseOrder purchaseOrderRepo, IProduct productRepo )
         {
             _purchaseOrderRepo = purchaseOrderRepo;
+            _productRepo = productRepo;
         }
         public IActionResult Index(string sortExpression = "", string searchText = "", int pg = 1, int pageSize = 5)
         {
@@ -37,7 +42,9 @@ namespace InventoryManagementSystem.Controllers
         public IActionResult Create()
         {
             var purchaseOrder = new PurchaseOrderHeader();
-            //ViewBag.ExchangeCurrencyId = GetCurrencies();
+            purchaseOrder.PurchaseOrderDetails.Add(new PurchaseOrderDetail() { Id = 1 });
+
+            ViewBag.ProductList = GetPrductList();
             return View(purchaseOrder);
         }
         [HttpPost]
@@ -68,5 +75,33 @@ namespace InventoryManagementSystem.Controllers
                 return RedirectToAction(nameof(Index));
             }
         }
+
+
+        #region Private Method
+
+        private List<SelectListItem> GetPrductList()
+        {
+            var lstItems = new List<SelectListItem>();
+
+            PaginatedList<Product> items = _productRepo.GetItems("Name", SortOrder.Ascending, "", 1, 1000);
+            lstItems = items.Select(ut => new SelectListItem()
+            {
+                Value = ut.Code,
+                Text = ut.Name
+            }).ToList();
+
+            var defItem = new SelectListItem()
+            {
+                Value = "",
+                Text = "-Select Product-"
+            };
+
+            lstItems.Insert(0, defItem);
+
+            return lstItems;
+        }
+
+
+        #endregion
     }
 }
